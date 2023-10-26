fe42547171272d276d3d5c1222615690746222cd
Hyeong Rok Oh <dhgudfhr1998@gmail.com>
2023-10-26 16:17:52 +0900

    created blockchain

---
+++ build/index.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Block {
+    constructor(prevHash, height, data, hash = "") {
+        this.prevHash = prevHash;
+        this.height = height;
+        this.data = data;
+        this.hash = Block.calculateHash(prevHash, height, data);
+    }
+    static calculateHash(prevHash, height, data) {
+        const toHash = `${prevHash}${height}${data}`;
+        return toHash;
+    }
+}
